9afb13fee129daad9abc3dde22d3bed421bfdd4f
MonicaGalicia <monica.garmz@gmail.com>
2016-10-26 18:34:25 +0000

    trello segunda versión

--- Trello/js/main.js
+++ Trello/js/main.js
@@ -8,26 +8,46 @@ var input=document.getElementById("input");
 
 function addLista(obj){
 	obj.style.backgroundColor="#E2E4E6";
-	//var formulario=document.createElement("form");
-	//formulario.setAttribute("id" , "form");
-	//formulario.appendChild(boton);
      tarjeta.appendChild(boton);
 
 }
 
 function presionarBtn(){
-	 var texto=document.createElement("h4");
+    var div=document.createElement("div");
+    div.setAttribute("id","div");
+	var texto=document.createElement("h5");
 	texto.appendChild(document.createTextNode(input.value));
 	var addTarjeta=document.createElement("a");
 	addTarjeta.setAttribute("id" , "enlace");
 	addTarjeta.appendChild(document.createTextNode("Añadir Lista"));
-	texto.appendChild(addTarjeta);
 
-	document.body.appendChild(texto);
+	div.appendChild(texto);
+	texto.appendChild(addTarjeta);
+	addTarjeta.appendChild(btn);
+	document.body.appendChild(div);
 
 	function clickEnlace(){
-	 var area=document.createElement("textarea");
-	 addTarjeta.appendChild(area);
+    
+     var divDos=document.createElement("div");
+     divDos.setAttribute("id","divdos");
+     var area=document.createElement("textarea");
+     area.setAttribute("id","area");
+     var btn=document.createElement("button");
+     btn.setAttribute("id","btn");
+     btn.innerHTML="Guardar";
+
+
+
+     divDos.appendChild(area);
+     divDos.appendChild(btn);
+    div.appendChild(divDos);
+    addTarjeta.parentElement.replaceChild(divDos,addTarjeta);
+
+	 //var area=document.createElement("textarea");
+	 //area.setAttribute("id","area");
+	 //addTarjeta.appendChild(area);
+	 //console.log( addTarjeta.parentElement);
+	 //addTarjeta.parentElement.replaceChild(area,addTarjeta);
 	
 }
 
